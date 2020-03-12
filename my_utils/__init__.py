246616fc8e138ceed61a4aab370328b0256393e7
mileswan <mileswan@wezhuiyi.com>
2020-03-12 13:47:58 +0800

    优化日志

--- my_utils/__init__.py
+++ my_utils/__init__.py
@@ -7,15 +7,20 @@ from .loader import *
 from .libs import *
 from .temp import *
 
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter(
-    '%(asctime)s [%(levelname)s %(name)s %(lineno)d] | %(message)s',
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s %(name)s %(lineno)d] | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
+
+logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+#
+# formatter = logging.Formatter(
+#     '%(asctime)s [%(levelname)s %(name)s %(lineno)d] | %(message)s',
+#     datefmt='%Y-%m-%d %H:%M:%S'
+# )
+# stream_handler = logging.StreamHandler()
+# stream_handler.setLevel(logging.INFO)
+# stream_handler.setFormatter(formatter)
+# logger.addHandler(stream_handler)
